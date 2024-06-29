5da47b6782b0739a68d4d84073a15b39fcb8962b
MabelisYT <mabeliscool1234@gmail.com>
2024-06-29 15:41:13 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,3 +1,3 @@
-wget https://github.com/MabelMedia-LLC/QuicArch/releases/download/Release/QuicArch -o ~/QuicArch
+curl -O https://github.com/MabelMedia-LLC/QuicArch/releases/download/Release/QuicArch -o ./QuicArch
 chmod +x ./QuicArch
 ./QuicArch
